21b6dfbc105400e508323c9815c2912110a5fa97
root <root@storage.(none)>
2012-11-08 09:38:48 +0800

    添加鼠标事件，给inputValidate方法添加返回值

--- validatebox/validatebox.js
+++ validatebox/validatebox.js
@@ -12,29 +12,44 @@ $(function () {
 			$('#'+this.options.id+'').bind('input',this.inputValidate);
 			$('#'+this.options.id+'').bind('focus',this.inputFocus);
 			$('#'+this.options.id+'').bind('blur',this.inputBlur);
+			$('#'+this.options.id+'').bind('mouseover',this.inputFocus);
+			$('#'+this.options.id+'').bind('mouseout',this.inputBlur);
 			this.render();
 		},
 		render : function() {//tip的页面元素
-			var $validatetip = $('<span class="validate_tip"></span>');
-			var p_x = $('#'+this.options.id+'').offset().left + $('#'+this.options.id+'').width() + 5;
-			$validatetip.insertAfter('#'+this.options.id+'');
-			$validatetip.css("left",p_x);
+			$('#' + this.options.id + '').after('<span class="validate_tip"></span>');
 		},
 		inputValidate:function(){
 			this.content = $('#'+this.options.id+'').val();
 			$('#'+this.options.id+'').next().text(this.options.tipmsg);
-			!this.options.reg_exp.test(this.content) ? $('#'+this.options.id+'').next().show():$('#'+this.options.id+'').next().hide();
+			if(this.options.reg_exp.test(this.content)){
+				$('#'+this.options.id+'').next().hide();
+				return true;
+			}else{
+				$('#'+this.options.id+'').next().show();
+				return false;
+			}
+			this.cssPosition();
 		},
 		inputFocus:function(){
 			if($('#'+this.options.id+'').val()){//有值的时候
 				this.inputValidate();
 			}else{
 				$('#'+this.options.id+'').next().text(this.options.reqmsg);
-			    $('#'+this.options.id+'').attr("required")?$('#'+this.options.id+'').next().show():$('#'+this.options.id+'').next().hide();	
+			    	$('#'+this.options.id+'').attr("required")?$('#'+this.options.id+'').next().show():$('#'+this.options.id+'').next().hide();
+			    	this.cssPosition();	
 			}
 		},
 		inputBlur:function(){
 			$('#'+this.options.id+'').next().hide();
+		},
+		cssPosition:function(){
+			var p_y = $('#' + this.options.id + '').offset().top;
+			var p_x = $('#' + this.options.id + '').offset().left + $('#' + this.options.id + '').width() + 5;
+			$('#' + this.options.id + '').next().css({
+				left : p_x,
+				top : p_y
+			});
 		}
 	});
 }());
